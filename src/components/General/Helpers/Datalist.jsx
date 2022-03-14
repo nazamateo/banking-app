37f06f8c918f5e648c98f7ea4aa37e75f50aea6b
naza mateo <nazamateo@gmail.com>
2022-03-15 02:03:31 +0800

    fix buggy datalist component

--- src/components/General/Helpers/Datalist.jsx
+++ src/components/General/Helpers/Datalist.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import {getBankAccounts} from "../../LocalStorage/LocalStorage";
 
 
-let getFromLocalStorage = getBankAccounts();
 const NameDataListGenerator = () =>{
-return getFromLocalStorage.map(users=>
+return getBankAccounts().map(users=>
 <option value={users.name}></option>
     )
 }
 
 const AccntNumDataListGenerator = () =>{
-    return getFromLocalStorage.map(users=>
+    return getBankAccounts().map(users=>
     <option value={users.accountNumber}></option>
         )
     }
